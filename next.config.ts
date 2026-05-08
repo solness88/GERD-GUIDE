65061a4e8115ee424a37d50fa09c80674887284d
solness88 <solness.stf@gmail.com>
2026-05-09 00:22:37 +0900

    next.config.tsを修正。AVIFまたはWebP形式に自動変換

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
